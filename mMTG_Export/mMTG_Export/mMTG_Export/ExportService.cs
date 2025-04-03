23a02b57227e0acd648790759c5047a6abebc6f5
gaganthakkar <gaganthakkar8@gmail.com>
2025-04-03 00:37:16 -0400

    change1

---
+++ mMTG_Export/mMTG_Export/mMTG_Export/ExportService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mMTG_Export
+{
+    public class ExportService
+    {
+        //Student student = new Student();
+
+                List<Student> students = new List<Student>
+        {
+            new Student { Id = 1, Name = "John" },
+            new Student { Id = 2, Name = "Doe" }
+        };
+
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
